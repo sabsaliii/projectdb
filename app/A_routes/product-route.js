0afac4b678bf71044175778060d4e3fe64be492a
cys41 <younseo42@gmail.com>
2022-06-13 02:42:28 +0900

    [윤서] 상단바 css및 검색창 css수정

--- app/A_routes/product-route.js
+++ app/A_routes/product-route.js
@@ -1,6 +1,8 @@
 const methodOverride=require('method-override');
 const bodyParser=require('body-parser');
 const ProductController = require('../B_controllers/product-controller');
+const {loginState} = require('../middleware/jwt');
+
 
 module.exports=router=>{
     router.use(bodyParser.json());
--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const express = require('express');
 const cookieParser=require('cookie-parser');
 const pool = require('./app/E_database/pool')
 const app = express()
-
+// const middleware = require('./app/middleware/jwt');
+const {loginState} = require('./app/middleware/jwt');
 
 init = async (req, res) => {
 await pool.init;
@@ -19,7 +20,10 @@ app.set('view engine','ejs');
 
 app.set('views',"./views");
 app.use(express.static(path.join(__dirname,'public')));
-app.get('/',(req,res)=>res.render('project_main'));
+app.get('/',(req,res,next)=>{
+    const loginstate = loginState(req,res,next);
+    console.log(loginstate);
+    return res.render('project_main',{login:loginstate})});
 
 require('./app/A_routes/product-route')(app)
 require('./app/A_routes/admin-route')(app)
